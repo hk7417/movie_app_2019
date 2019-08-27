27d86db4ea906e088649540eda243b487790b11f
Hyun Gyeong Park <hk7417@naver.com>
2019-08-27 18:42:31 +0900

    Updates

---
+++ precache-manifest.6cbfb3e45697596de8bb58538331b219.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60dbdb2b962202f63abccad590dda7d4",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "73e2bc5799ba3856d031",
+    "url": "/movie_app_2019/static/css/main.fecc8da0.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "73e2bc5799ba3856d031",
+    "url": "/movie_app_2019/static/js/main.10d72574.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
